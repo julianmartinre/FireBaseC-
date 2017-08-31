7cca9bbdfdd58c94e226d7a0404508bf86844a18
julianmartinre <julian.martin.93@gmail.com>
2017-08-30 21:50:26 -0300

    initial commit

---
+++ ejemplo.firebase.core/Class1.cs
@@ -0,0 +1,67 @@
+﻿using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo.firebase.core
+{
+
+
+    public class ContextoFirebase : ICrudUsuario
+    {
+
+        IFirebaseConfig config;
+        IFirebaseClient client;
+
+        string url = "https://diploma-uai.firebaseio.com/";
+        string secret = "ZSjNzguVr7miIxSHACZVJAFoOwnMLJDIRmgwHulp";
+
+        public ContextoFirebase()
+        {
+            config = new FirebaseConfig()
+            {
+                AuthSecret = secret,
+                BasePath = url
+            };
+            client = new FirebaseClient(config);
+
+        }
+
+        public void Guardar(Usuario c)
+        {
+            client.Push<Usuario>("Usuario/", c);
+        }
+
+        public Usuario Obtener(object id)
+        {
+            return client.Get("Usuario/" + id).ResultAs<Usuario>();
+              
+        }
+
+        public IEnumerable<Usuario> ObtenerTodos()
+        {
+            Dictionary<object, Usuario> res = client.Get("Usuario").ResultAs<Dictionary<object, Usuario>>();
+
+            return res.Values.AsEnumerable<Usuario>();
+        }
+    }
+
+
+    interface ICrudUsuario
+    {
+       void Guardar(Usuario c);
+        Usuario Obtener(object id);
+
+        IEnumerable<Usuario> ObtenerTodos();
+    }
+
+    public class Usuario
+    {
+     public string Username { get; set; }
+     public string Password{ get; set; }
+    }
+}
---
+++ ejemplo.firebase.ui/Form1.cs
@@ -0,0 +1,36 @@
+﻿using ejemplo.firebase.core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejemplo.firebase.ui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Usuario c = new Usuario()
+            {
+                Username = textBox1.Text,
+                Password = textBox2.Text
+            };
+            new ContextoFirebase().Guardar(c);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            IEnumerable<Usuario> users = new ContextoFirebase().ObtenerTodos();
+        }
+    }
+}
